42ac546cc668b95c86a0e9e9154d2db63df98170
Erik Lovlie <eriklovlie@gmail.com>
2015-01-25 12:54:33 +0100

    Enable automatic desktop save and restore.

--- init.el
+++ init.el
@@ -23,7 +23,8 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default))))
+    ("6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
+ '(desktop-save-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
